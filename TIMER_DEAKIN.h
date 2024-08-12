4aa4f98dc366836ddb12e50a4220b0c304b53e88
mitchellday6 <mitchell.day.6@gmail.com>
2024-08-12 20:50:24 +1000

    Update TIMER_DEAKIN.h

--- TIMER_DEAKIN.h
+++ TIMER_DEAKIN.h
@@ -1,28 +1,22 @@
-#pragma once
-
+// timer_deakin.h
 #ifndef TIMER_DEAKIN_H
 #define TIMER_DEAKIN_H
 
 #include <Arduino.h>
 
+class TIMER_DEAKIN
+{
+private:
+    volatile uint32_t tick_count; // Track the number of ticks (32bit as this could be a large number)
+    volatile uint16_t tcNo; // the selected timer number
+    TcCount16* TC; // the selected timer number
+    volatile uint16_t res; //resolution of counter
+    volatile uint32_t endTick; //the end tick (32bit as this could be a large number)
 
-class TIMER_DEAKIN {
-    private:
-        //current count value of each timer
-        uint16_t count3;
-        uint16_t count4;
-        uint16_t count5;
-        //the last count value before reset
-        uint16_t endCount3;
-        uint16_t endCount4;
-        uint16_t endCount5;
-    public:
-        TIMER_DEAKIN();
-        bool config_timer(int timerNo, int countStart, int countEnd, int timerResolution);
-        uint getTC3_count();
-        uint getTC4_count();
-        uint getTC5_count();
-        bool wait(uint waitTime);
+public:
+    bool config_timer(uint8_t timer_num, uint16_t start_val, uint32_t end_val, uint16_t resolution);
+    uint16_t getTC_count();
+    void wait(uint32_t period_in_0_1ms);
 };
 
 #endif
