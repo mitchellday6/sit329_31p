1ba80c71edc8361a91ab4fd083f0b907e27df072
mitchellday6 <mitchell.day.6@gmail.com>
2024-08-12 20:50:43 +1000

    Update 3-1p_mitchell.ino

--- 3-1p_mitchell.ino
+++ 3-1p_mitchell.ino
@@ -4,24 +4,20 @@
 GPIO_DEAKIN myGpio;
 TIMER_DEAKIN myTimer;
 bool isOn;
-const uint FLASH_TIME = 50000; //Flash times in 10ths of a millisecond
+uint32_t FLASH_TIME; //Flash times in 10ths of a millisecond
 double iTimer; //tracks the timers current time in 10ths of a millisecond
 
 void setup() {
   Serial.begin(9600);
   myGpio.Config_GPIO('A', 17, OUTPUT);
-  // iTimer = 0; // init to 0
+  FLASH_TIME = 65005;
 }
 
 void loop() {
-  Serial.println(1);
   myGpio.Write_GPIO('A', 17, HIGH);
   isOn = myGpio.Read_GPIO('A', 17);
-  
-  bool finished = myTimer.wait(FLASH_TIME);
-  Serial.println(finished);
+  myTimer.wait(FLASH_TIME);
   myGpio.Write_GPIO('A', 17, LOW);
   isOn = myGpio.Read_GPIO('A', 17);
   myTimer.wait(FLASH_TIME);
 }
-
